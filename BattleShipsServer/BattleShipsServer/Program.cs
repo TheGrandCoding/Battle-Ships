fd74ed9aa9f2f829f1b8ad18741be7a5a692ec94
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2019-10-01 14:50:21 +0100

    Started Server

--- BattleShipsServer/BattleShipsServer/Program.cs
+++ BattleShipsServer/BattleShipsServer/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
+using System.Net;
+using System.Net.Sockets;
 
 namespace BattleShipsServer
 {
     class Program
     {
+        public static TcpListener Server;
+        public static IPAddress ip;
         static void Main(string[] args)
         {
+            ip = Getipadress();
+            Start();
         }
+        private static void Start()
+        {
+            Server = new TcpListener(IPAddress.Loopback,777);
+        }
+        private static IPAddress Getipadress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;//ipv4
+                }
+            }
+            return null;
+        }
+
     }
 }

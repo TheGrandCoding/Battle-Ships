51be9cf4a4cf812e11a2dd2e8cd99d80758d6ca4
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2019-10-01 16:59:46 +0100

    AddedAllowClientsToConnect
    Clients can now connect

--- BattleShipsServer/BattleShipsServer/Program.cs
+++ BattleShipsServer/BattleShipsServer/Program.cs
@@ -16,13 +16,42 @@ namespace BattleShipsServer
         static void Main(string[] args)
         {
             ip = Getipadress();
+            Console.WriteLine(ip.ToString());
             Start();
+            Console.ReadLine();
         }
-        private static void Start()
+        static void Start()
         {
             Server = new TcpListener(IPAddress.Loopback,777);
+            Server.Start();
+            while (true)
+            {
+                TcpClient newcon = Server.AcceptTcpClient();
+                Console.WriteLine("Accepted V4 connection from " + ((IPEndPoint)newcon.Client.RemoteEndPoint).Address.ToString());
+                Thread nu = new Thread(() => NewUser(newcon));
+                nu.Start();
+            }
         }
-        private static IPAddress Getipadress()
+        static void NewUser(TcpClient c)
+        {
+            Player p = new Player();
+            p.client = c;
+            Thread RD = new Thread(() => RecieveData(p));
+        }
+        static void RecieveData(Player p)
+        {
+            TcpClient clt = p.client;
+            string data = "";
+            NetworkStream RecieveDataStream = clt.GetStream();
+            byte[] bytes = new byte[256];
+            int i;
+            if ((i = RecieveDataStream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                // recieve username and game they want to join
+            }
+        }
+        static IPAddress Getipadress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)

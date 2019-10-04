9be1537284ce21e3c816fb91ba41f3106bd3aff2
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2019-10-04 11:55:40 +0100

    Added Logging
    Log Function in program.cs

--- BattleShipsServer/BattleShipsServer/Program.cs
+++ BattleShipsServer/BattleShipsServer/Program.cs
@@ -7,14 +7,22 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
+using System.IO;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+
 namespace BattleShipsServer
 {
     class Program
     {
         public static TcpListener Server;
         public static IPAddress ip;
+        public static string LogName;
         static void Main(string[] args)
         {
+            MakeLog();
+            Log("Start");
             ip = Getipadress();
             Console.WriteLine(ip.ToString());
             Start();
@@ -37,6 +45,7 @@ namespace BattleShipsServer
             Player p = new Player();
             p.client = c;
             Thread RD = new Thread(() => RecieveData(p));
+            RD.Start();
         }
         static void RecieveData(Player p)
         {
@@ -45,10 +54,15 @@ namespace BattleShipsServer
             NetworkStream RecieveDataStream = clt.GetStream();
             byte[] bytes = new byte[256];
             int i;
-            if ((i = RecieveDataStream.Read(bytes, 0, bytes.Length)) != 0)
+            while (true)
             {
-                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                // recieve username and game they want to join
+                if ((i = RecieveDataStream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine(data);
+                    Log("hi");
+                    // recieve username and game they want to join
+                }
             }
         }
         static IPAddress Getipadress()
@@ -63,6 +77,22 @@ namespace BattleShipsServer
             }
             return null;
         }
-
+        static void Log(string message)
+        {
+            StreamWriter swAppend = File.AppendText(LogName);
+            swAppend.WriteLine("["+DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString()+"]"+" - "+message);
+            swAppend.Close();
+        }
+        static void MakeLog()
+        {
+            if (!Directory.Exists("Logs/"))
+            {
+                Directory.CreateDirectory("Logs");
+            }
+            LogName = $"logs/" + DateTime.Today.Day.ToString() + " " + DateTime.Today.Month.ToString() + " & " + DateTime.Now.Hour.ToString() + ";" + DateTime.Now.Minute.ToString() +".txt";
+            StreamWriter swNew = File.CreateText(LogName);
+            swNew.Close();
+        }
     }
 }
+
